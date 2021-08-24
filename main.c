fd124a54f5864d863d09f9f1f96a8cd76dea37ab
Chbraga <chab09@hotmail.com>
2021-08-24 15:07:42 -0300

    Nova base para o jogo, dasher.cpp e primeiro_jogo são usados somente como referência e as texturas do dasher n consegui botar pra funcionar

--- main.c
+++ main.c
@@ -1,95 +1,43 @@
 #include "raylib.h"
 
+typedef struct 
+{
+    Rectangle rec;
+    Vector2 pos;
+    int frame;
+    float updateTime;
+    float runningTime;
+}AnimData; // Toda coisa animada deve ter um desses pra simplificar o cabeçalho
+
+bool isOnGround(AnimData data, int windowHeight) // check se tá no chão
+{
+    return data.pos.y >= windowHeight - data.rec.height;
+}
+
 int main(void){
     // Janela
     int width = 800;
     int height = 450;
     InitWindow(width, height, "Titulo desse ngc");
 
-    // Circulo 
-    int circle_x = width/3;
-    int circle_y = height/3;
-    int circle_radius = 25;
+    
 
-    // Cantos do circulo 
-    int l_circle_x = circle_x - circle_radius;
-    int r_circle_x = circle_x + circle_radius;
-    int u_circle_y = circle_y - circle_radius;
-    int b_circle_y = circle_y + circle_radius;
+    // Gravidade 
+    const int gravity = 500;
 
-    // Axe coordinates
-    int axe_x = 300;
-    int axe_y = 0;
-    int axe_length = 50;
-
-    int direction = 10;
-
-    // Axe cantos
-    int l_axe_x = axe_x;
-    int r_axe_x = axe_x + axe_length;
-    int u_axe_y = axe_y;
-    int b_axe_y = axe_y + axe_length; 
-
-    bool collision_with_axe = 
-        (b_axe_y >= u_circle_y) && 
-        (u_axe_y <= b_circle_y) && 
-        (l_axe_x <= r_circle_x) && 
-        (r_axe_x >= l_circle_x);
-
+    
     SetTargetFPS(60);
 
     while(!WindowShouldClose()){
         BeginDrawing();
         ClearBackground(WHITE); 
         
-        if(collision_with_axe){
+        if(){
             DrawText("Game over!", 400, 200, 20, RED); 
         }else{
-
-            // update cantos
-            l_circle_x = circle_x - circle_radius;
-            r_circle_x = circle_x + circle_radius;
-            u_circle_y = circle_y - circle_radius;
-            b_circle_y = circle_y + circle_radius;
-
-            l_axe_x = axe_x;
-            r_axe_x = axe_x + axe_length;
-            u_axe_y = axe_y;
-            b_axe_y = axe_y + axe_length; 
-
-            // Update collision
-            collision_with_axe = 
-                (b_axe_y >= u_circle_y) && 
-                (u_axe_y <= b_circle_y) && 
-                (l_axe_x <= r_circle_x) && 
-                (r_axe_x >= l_circle_x);
-
-
             // Logica start
 
-            DrawCircle(circle_x, circle_y, circle_radius, BLUE);
-            DrawRectangle(axe_x, axe_y, axe_length, axe_length, RED);
-
-            axe_y += direction;
-            if( axe_y > height || axe_y < 0){
-                direction = -direction;
-            }
-
-            if(IsKeyDown(KEY_D) && circle_x < width){
-                circle_x += 10;
-            }
-            if(IsKeyDown(KEY_A) && circle_x > 0){
-                circle_x -= 10;
-            }
-            if(IsKeyDown(KEY_W)){
-                circle_y -= 10;
-            }
-            if(IsKeyDown(KEY_S)){
-                circle_y += 10;
-            }
-
         }
-
 
         // Logica end
         EndDrawing();
