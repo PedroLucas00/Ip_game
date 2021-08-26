66b07f8b75950417b487dfbc0891ffaf792c1afe
Chbraga <chab09@hotmail.com>
2021-08-26 16:53:21 -0300

    Dessa vez vai dar  certo glr, fazendo a base do jogo novamente

--- main.c
+++ main.c
@@ -42,165 +42,36 @@ int main()
     scarfyData.frame = 0;
     scarfyData.updateTime = 1.0/12.0;
     scarfyData.runningTime = 0.0;
-    DrawRectangle();
 
-    // is the rectanlge in the air?
-    bool isInAir{};
-    // jump velocity (pixels/second)
-    const int jumpVel{-600};
-
-    int velocity{0};
-
-    Texture2D background = LoadTexture("textures/far-buildings.png");
-    float bgX{};
-    Texture2D midground = LoadTexture("textures/back-buildings.png");
-    float mgX{};
-    Texture2D foreground = LoadTexture("textures/foreground.png");
-    float fgX{};
-
-    bool collision{};
-
+    
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
-        // delta time (time since last frame)
-        const float dT{GetFrameTime()};
 
         // start drawing
         BeginDrawing();
         ClearBackground(WHITE);
+        DrawRectangle(scarfyData.pos.x, scarfyData.pos.y, scarfyData.rec.width, scarfyData.rec.height, RED);
 
-        // Scroll background
-        bgX -= 20 * dT;
-        if (bgX <= -background.width*2)
+        // controles basicos 
+        if (IsKeyDown(KEY_D))
         {
-            bgX = 0.0;
+            scarfyData.pos.x += 10;
         }
-        // Scroll the midground
-        mgX -= 40 * dT;
-        if (mgX <= -midground.width*2)
+        if (IsKeyDown(KEY_A))
         {
-            mgX = 0.0;
-        }
-        // Scroll the foreground
-        fgX -= 80 * dT;
-        if (fgX <= -foreground.width*2)
+            scarfyData.pos.x -= 10;
+        }if (IsKeyDown(KEY_W))
         {
-            fgX = 0.0;
-        }
-
-        // draw the background
-        Vector2 bg1Pos{bgX, 0.0};
-        DrawTextureEx(background, bg1Pos, 0.0, 2.0, WHITE);
-        Vector2 bg2Pos{bgX + background.width*2, 0.0};
-        DrawTextureEx(background, bg2Pos, 0.0, 2.0, WHITE);
-
-        // draw the midground
-        Vector2 mg1Pos{mgX, 0.0};
-        DrawTextureEx(midground, mg1Pos, 0.0, 2.0, WHITE);
-        Vector2 mg2Pos{mgX + midground.width*2, 0.0};
-        DrawTextureEx(midground, mg2Pos, 0.0, 2.0, WHITE);
-
-        // draw the foreground
-        Vector2 fg1Pos{fgX, 0.0};
-        DrawTextureEx(foreground, fg1Pos, 0.0, 2.0, WHITE);
-        Vector2 fg2Pos{fgX + foreground.width*2, 0.0};
-        DrawTextureEx(foreground, fg2Pos, 0.0, 2.0, WHITE);
-
-        // perform ground check
-        if (isOnGround(scarfyData, windowDimensions[1]))
+            scarfyData.pos.y -= 10;
+        }if (IsKeyDown(KEY_S))
         {
-            // rectangle is on the ground
-            velocity = 0;
-            isInAir = false;
-        }
-        else
-        {
-            // rectangle is in the air
-            velocity += gravity * dT;
-            isInAir = true;
-        }
-
-        // jump check
-        if (IsKeyPressed(KEY_SPACE) && !isInAir)
-        {
-            velocity += jumpVel;
-        }
-
-
-        for (int i = 0; i < sizeOfNebulae; i++)
-        {
-            // update the position of each nebula
-            nebulae[i].pos.x += nebVel * dT;
-        }
-
-        // update finishLine
-        finishLine += nebVel * dT;
-
-        // update scarfy position
-        scarfyData.pos.y += velocity * dT;
-
-        // update scarfy's animation frame
-        if (!isInAir)
-        {
-            scarfyData = updateAnimData(scarfyData, dT, 5);
-        }
-
-        for (int i = 0; i < sizeOfNebulae; i++)
-        {
-            nebulae[i] = updateAnimData(nebulae[i], dT, 7);
-        }
-
-        for (AnimData nebula : nebulae)
-        {
-            float pad{50};
-            Rectangle nebRec{
-                nebula.pos.x + pad,
-                nebula.pos.y + pad,
-                nebula.rec.width - 2*pad,
-                nebula.rec.height - 2*pad
-            };
-            Rectangle scarfyRec{
-                scarfyData.pos.x,
-                scarfyData.pos.y,
-                scarfyData.rec.width,
-                scarfyData.rec.height
-            };
-            if (CheckCollisionRecs(nebRec, scarfyRec))
-            {
-                collision = true;
-            }
-        }
-
-        if (collision)
-        {
-            // lose the game
-            DrawText("Game Over!", windowDimensions[0]/4, windowDimensions[1]/2, 40, RED);
-        }
-        else if (scarfyData.pos.x >= finishLine)
-        {
-            // win the game
-            DrawText("You Win!", windowDimensions[0]/4, windowDimensions[1]/2, 40, RED);
-        }
-        else
-        {
-            for (int i = 0; i < sizeOfNebulae; i++)
-            {
-                // draw nebula
-                DrawTextureRec(nebula, nebulae[i].rec, nebulae[i].pos, WHITE);
-            }
-
-            // draw scarfy
-            DrawTextureRec(scarfy, scarfyData.rec, scarfyData.pos, WHITE);
+            scarfyData.pos.y += 10;
         }
 
         // stop drawing
         EndDrawing();
     }
-    UnloadTexture(scarfy);
-    UnloadTexture(nebula);
-    UnloadTexture(background);
-    UnloadTexture(midground);
-    UnloadTexture(foreground);
+     
     CloseWindow();
 }
