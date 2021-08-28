927025feb8be5e4aadd5ed4dce81cad2b83431d5
Chbraga <chab09@hotmail.com>
2021-08-28 15:18:37 -0300

    Changes in main.c, working on shoot function

--- main.c
+++ main.c
@@ -1,8 +1,24 @@
 #include "raylib.h"
 
-// Define functions
-void menu(int width, int height);
-void gameplay(int width, int height);
+#define MAX_SHOTS 5 
+// Game screens
+typedef enum{
+    MENU,
+    GAMEPLAY,
+    RESUME,
+    DEATH,
+    QUIT,
+}screen;
+
+
+/* typedef struct{
+    Vector2 speed; 
+    bool active;
+    Color color;
+    Vector2 position;
+    int lifespawn;
+    float radius;
+}shoot; */
 
 typedef struct 
 {
@@ -18,9 +34,25 @@ bool isOnGround(AnimData data, int windowHeight)
     return data.pos.y >= windowHeight - data.rec.height;
 }
 
-void gameplay(int width, int height){
+// atirando para a direita 
+typedef struct{
+    Vector2 speed; 
+    bool active;
+    Color color;
+    Vector2 position;
+    int lifespawn;
+    float radius;
+}shoot;
 
-    int windowDimensions[2];
+// Define functions
+int menu(int width, int height);
+int gameplay(int width, int height);
+int pause(int width, int height);
+int death(int width, int height);
+
+// Gameplay work flow
+int gameplay(int width, int height){
+int windowDimensions[2];
     windowDimensions[0] = width; // x
     windowDimensions[1] = height; // y
 
@@ -28,9 +60,13 @@ void gameplay(int width, int height){
     // acceleration due to gravity (pixels/s)/s
     float gravity = 0;
     float acceleration = 1.5;
-    int temp;
     
-
+    // num tiros
+    shoot Shoot[MAX_SHOTS] = {0};
+    
+    // direção projétil
+    // bool shoot_right; // se 1, indo p/ direita, se 0, indo para a esquerda
+    
 
     // player variables
     AnimData scarfyData;
@@ -44,16 +80,22 @@ void gameplay(int width, int height){
     scarfyData.updateTime = 1.0/12.0;
     scarfyData.runningTime = 0.0;
 
+    /* for(int i = 0; i < MAX_SHOTS; i++){
+        shoot Shoot[i];
+        
+    } */
+    int shoot_right = 0;
+
     
     while (!WindowShouldClose())
     {
-        
         // start drawing
         BeginDrawing();
         ClearBackground(WHITE);
         DrawRectangle(scarfyData.pos.x, scarfyData.pos.y, scarfyData.rec.width, scarfyData.rec.height, RED);
         // gravidade
-        
+        for(int i = 0; i < MAX_SHOTS; i++){
+        }
         if(!isOnGround(scarfyData, windowDimensions[1])){
             scarfyData.pos.y += gravity;
         };
@@ -61,10 +103,12 @@ void gameplay(int width, int height){
         // controles basicos 
         if (IsKeyDown(KEY_D))
         {
+            shoot_right = 1;
             scarfyData.pos.x += 10;
         }
         if (IsKeyDown(KEY_A))
         {
+            shoot_right = 0;
             scarfyData.pos.x -= 10;
         }
         if(IsKeyDown(KEY_SPACE) && isOnGround(scarfyData, windowDimensions[1])){
@@ -85,15 +129,139 @@ void gameplay(int width, int height){
         gravity += acceleration;
 
         // Tiro do player
-        
+
+        for(int i = 0; i < MAX_SHOTS; i++){ // no sé que se passa
+            Shoot[i].position = (Vector2){scarfyData.pos.x + 30, scarfyData.pos.y + 30};
+            Shoot[i].speed.x = 3;
+            Shoot[i].radius = 10;
+            Shoot[i].active = false;
+            Shoot[i].color = BLUE;
+            Shoot[i].lifespawn = 0;
+        }
+        if (IsKeyDown(KEY_F))
+        {
+             for(int i = 0; i < MAX_SHOTS; i++){
+                 if(!Shoot[i].active){
+                     Shoot[i].position = (Vector2){scarfyData.pos.x + 30, scarfyData.pos.y + 30};
+                     Shoot[i].active = true;
+                     break;
+                 }
+             }
+        }
+        for(int i = 0; i < MAX_SHOTS; i++){
+            if(Shoot[i].active){
+                Shoot[i].position.x += 3;
+                Shoot[i].lifespawn++;
+                if(Shoot[i].position.x >= 800){
+                    Shoot[i].active = false;
+                }
+                if(Shoot[i].active){
+                    DrawCircleV(Shoot[i].position, Shoot[i].radius, BLUE);
+                }
+                if(Shoot[i].lifespawn >= 80){
+                    Shoot[i].position = (Vector2){scarfyData.pos.x + 30, scarfyData.pos.y + 30};
+                    Shoot[i].speed = (Vector2){0, 0};
+                    Shoot[i].lifespawn = 0;
+                    Shoot[i].active = false;
+                }
+            }
+        }
 
         // stop drawing
         EndDrawing();
     }
 }
 
-void menu(int width, int height){
-    
+// Pause menu
+int pause(int width, int height){
+    // Pause Menu Variables
+    int selected_pausemenu_option = 1;
+    // Define PAUSE font size
+    int PAUSE_fontsize = 60;
+    // Define text font size
+    int TEXT_fontsize = 46;
+    // Define Pause Background Image
+    Texture2D pause_background = LoadTexture("images/pause_menu_background.png");
+    // Loop while waiting for a ENTER press
+    while(!IsKeyReleased(KEY_ENTER)){
+        // Menu Work Flow
+        if(selected_pausemenu_option==4) selected_pausemenu_option = 1;
+        if(selected_pausemenu_option==0) selected_pausemenu_option = 3;
+        if(IsKeyReleased(KEY_UP)) selected_pausemenu_option--;
+        if(IsKeyReleased(KEY_DOWN)) selected_pausemenu_option++;
+        // Menu Drawing
+        BeginDrawing();
+        DrawTexture(pause_background, 416, 218, WHITE);
+        DrawText("PAUSE", (width-width/2)-PAUSE_fontsize, height-(height/4), PAUSE_fontsize, WHITE);
+        if(selected_pausemenu_option==1){
+            DrawText("RESUME", 545, 268, TEXT_fontsize, RED);
+            DrawText("GO TO MENU", 499, 339, TEXT_fontsize, WHITE);
+            DrawText("QUIT", 586, 411, TEXT_fontsize, WHITE);
+        }
+        else if(selected_pausemenu_option==2){
+            DrawText("RESUME", 545, 268, TEXT_fontsize, WHITE);
+            DrawText("GO TO MENU", 499, 339, TEXT_fontsize, RED);
+            DrawText("QUIT", 586, 411, TEXT_fontsize, WHITE);
+        }
+        else if(selected_pausemenu_option==3){
+            DrawText("RESUME", 545, 268, TEXT_fontsize, WHITE);
+            DrawText("GO TO MENU", 499, 339, TEXT_fontsize, WHITE);
+            DrawText("QUIT", 586, 411, TEXT_fontsize, RED);
+        }
+        EndDrawing();
+    }
+    // After press ENTER key
+    if(selected_pausemenu_option==1) return RESUME;
+    if(selected_pausemenu_option==2) return MENU;
+    if(selected_pausemenu_option==3) return QUIT;
+}
+
+// Death menu
+int death(int width, int height){
+    // Death Menu Variables
+    int selected_deathmenu_option = 1;
+    // Define GAMEOVER font size
+    int GAMEOVER_fontsize = 60;
+    // Define text font size
+    int TEXT_fontsize = 45;
+    // Define Death Background Image
+    Texture2D death_background = LoadTexture("images/death_background.png");
+    // Loop while waiting for a ENTER press
+    while(!IsKeyReleased(KEY_ENTER)){
+        // Menu Work Flow
+        if(selected_deathmenu_option==4) selected_deathmenu_option = 1;
+        if(selected_deathmenu_option==0) selected_deathmenu_option = 3;
+        if(IsKeyReleased(KEY_UP)) selected_deathmenu_option--;
+        if(IsKeyReleased(KEY_DOWN)) selected_deathmenu_option++;
+        // Menu Drawing
+        BeginDrawing();
+        DrawTexture(death_background, 0, 0, WHITE);
+        DrawText("GAME OVER", width-width/3, height-(height/6), GAMEOVER_fontsize, RED);
+        if(selected_deathmenu_option==1){
+            DrawText("TRY AGAIN", 153, 372, TEXT_fontsize, RED);
+            DrawText("GO TO MENU", 115, 442, TEXT_fontsize, WHITE);
+            DrawText("QUIT", 284, 581, TEXT_fontsize, WHITE);
+        }
+        else if(selected_deathmenu_option==2){
+            DrawText("TRY AGAIN", 153, 372, TEXT_fontsize, WHITE);
+            DrawText("GO TO MENU", 115, 442, TEXT_fontsize, RED);
+            DrawText("QUIT", 284, 581, TEXT_fontsize, WHITE);
+        }
+        else if(selected_deathmenu_option==3){
+            DrawText("TRY AGAIN", 153, 372, TEXT_fontsize, WHITE);
+            DrawText("GO TO MENU", 115, 442, TEXT_fontsize, WHITE);
+            DrawText("QUIT", 284, 581, TEXT_fontsize, RED);
+        }
+        EndDrawing();
+    }
+    // After press ENTER key
+    if(selected_deathmenu_option==1) return GAMEPLAY;
+    if(selected_deathmenu_option==2) return MENU;
+    if(selected_deathmenu_option==3) return QUIT;
+}
+
+// Game menu
+int menu(int width, int height){
     // Menu Variables
     int selected_menu_option = 1;
     int text_menu_size = 45;
@@ -120,25 +288,30 @@ void menu(int width, int height){
         EndDrawing();
     }
     // After press ENTER key
-    if(selected_menu_option==1) gameplay(width, height);
-    if(selected_menu_option==2) CloseWindow();
+    if(selected_menu_option==1) return GAMEPLAY;
+    if(selected_menu_option==2) return QUIT;
 }
 
 int main(void){
-    // array with window dimensions
-    int windowDimensions[2];
-    windowDimensions[0] = 1280; // x
-    windowDimensions[1] = 720; // y
-
-    InitWindow(windowDimensions[0], windowDimensions[1], "A code adventure");
-
+    // Game Window
+    const int width = 1280;
+    const int height = 720;
+    InitWindow(width, height, "A Code Adventure");
     
     // Game FPS
     SetTargetFPS(60);
     
-    // Menu
-    menu(windowDimensions[0], windowDimensions[1]);
+    // Main Variables
+    int current_screen = MENU;
+    
+    // Loop game screen change
+    while(current_screen!=QUIT){
+        if(current_screen==MENU) current_screen = menu(width, height);
+        else if(current_screen==GAMEPLAY) current_screen = gameplay(width, height);
+        else if(current_screen==DEATH) current_screen = death(width, height);
+    }
     
     // THE END
     CloseWindow();
+    return 0;
 }
