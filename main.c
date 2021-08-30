abcccc888523a8d194f759b43e8ffc2030c1a8f5
NicBrito <nicolas_brito_barros@hotmail.com>
2021-08-30 20:01:45 -0300

    feat/Indentation correction

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "raylib.h"
 
-#define MAX_SHOTS 5 
-#define MAX_SHOTS_LEFT 5 
+#define MAX_SHOTS 5
+#define MAX_SHOTS_LEFT 5
 
 // Game screens
 typedef enum{
@@ -12,9 +12,7 @@ typedef enum{
     QUIT,
 }screen;
 
-
-typedef struct 
-{
+typedef struct {
     Rectangle rec;
     Vector2 pos;
     int frame;
@@ -22,10 +20,7 @@ typedef struct
     float runningTime;
 }AnimData;
 
-bool isOnGround(AnimData playerData, int windowHeight)
-{
-    return playerData.pos.y >= windowHeight - playerData.rec.height;
-}
+bool isOnGround(AnimData playerData, int windowHeight) {return playerData.pos.y >= windowHeight - playerData.rec.height;}
 
 // Running animation reference
 /*animate(AnimData playerData, int initAnim, int endAnim,int currentFrame, int framesSpeed, int framesCounter) {
@@ -43,9 +38,9 @@ bool isOnGround(AnimData playerData, int windowHeight)
         }
 }*/
 
-// atirando para a direita 
-typedef struct{
-    Vector2 speed; 
+// atirando para a direita
+typedef struct {
+    Vector2 speed;
     bool active;
     Color color;
     Vector2 position;
@@ -54,8 +49,8 @@ typedef struct{
 }shoot;
 
 // atirando para a esquerda
-typedef struct{
-    Vector2 speed_left; 
+typedef struct {
+    Vector2 speed_left;
     bool active_left;
     Color color_left;
     Vector2 position_left;
@@ -63,11 +58,9 @@ typedef struct{
     float radius_left;
 }shoot_left;
 
-typedef struct{
+typedef struct {
     Rectangle rec;
 }level_rec;
-
-
 
 // Define functions
 int menu(int width, int height);
@@ -77,14 +70,25 @@ int death(int width, int height);
 
 // Gameplay work flow
 int gameplay(int width, int height, int framesSpeed, int framesCounter){
-int windowDimensions[2];
+    // Starting audio device
+    InitAudioDevice();
+    // Define Gameplay Audio
+    Sound gameplay_background_audio = LoadSound("audio/gameplay_background.wav");
+    // Define Gameplay Background Image
+    Texture2D gameplay_background = LoadTexture("images/gameplay_background.png");
+    // Set Gameplay Volume
+    SetSoundVolume(gameplay_background_audio, 0.4);
+    // Play Gameplay Sound
+    PlaySound(gameplay_background_audio);
+    
+    int windowDimensions[2];
 
     windowDimensions[0] = width; // x
     windowDimensions[1] = height; // y
 
-     // variables to choose between animations intended
-     int initAnim, endAnim;
-     int currentFrame = 0;
+    // variables to choose between animations intended
+    int initAnim, endAnim;
+    int currentFrame = 0;
 
     // acceleration due to gravity (pixels/s)/s
     float gravity = 0;
@@ -98,10 +102,10 @@ int windowDimensions[2];
     // bool shoot_right; // se 1, indo p/ direita, se 0, indo para a esquerda
 
     // Player Animation and variables
-    Texture2D  player = LoadTexture("./Sprites/player_sheet.png"); 
+    Texture2D  player = LoadTexture("./Sprites/player_sheet.png");
     
     AnimData playerData;
-    playerData.rec.width = (float)(player.width/19); 
+    playerData.rec.width = (float)(player.width/19);
     playerData.rec.height = (float)player.height;
     playerData.rec.x = 0.0f;
     playerData.rec.y = 0.0f;
@@ -115,7 +119,7 @@ int windowDimensions[2];
     Texture2D  tiro = LoadTexture("./Sprites/malloc_sheet_upscale.png");
 
     AnimData tiroData;
-    tiroData.rec.width = (float)(tiro.width/2); 
+    tiroData.rec.width = (float)(tiro.width/2);
     tiroData.rec.height = (float)tiro.height;
     tiroData.rec.x = 0.0f;
     tiroData.rec.y = 0.0f;
@@ -129,7 +133,7 @@ int windowDimensions[2];
     Texture2D  memory = LoadTexture("./Sprites/memory_sheet.png");
 
     AnimData memoryData;
-    memoryData.rec.width = (float)(memory.width/7); 
+    memoryData.rec.width = (float)(memory.width/7);
     memoryData.rec.height = (float)memory.height;
     memoryData.rec.x = 0.0f;
     memoryData.rec.y = 0.0f;
@@ -142,34 +146,44 @@ int windowDimensions[2];
     level_rec prim;
 
     for(int i = 0; i < MAX_SHOTS; i++){ // no sé que se passa
-            Shoot[i].position = (Vector2){playerData.pos.x + 30, playerData.pos.y + 30};
-            Shoot[i].speed.x = 3;
-            Shoot[i].radius = 10;
-            Shoot[i].active = false;
-            Shoot[i].color = BLUE;
-            Shoot[i].lifespawn = 0;
+        Shoot[i].position = (Vector2){playerData.pos.x + 30, playerData.pos.y + 30};
+        Shoot[i].speed.x = 3;
+        Shoot[i].radius = 10;
+        Shoot[i].active = false;
+        Shoot[i].color = BLUE;
+        Shoot[i].lifespawn = 0;
     }
 
     for(int i = 0; i < MAX_SHOTS_LEFT; i++){ // no sé que se passa esquérdita
-            Shoot_left[i].position_left = (Vector2){playerData.pos.x + 30, playerData.pos.y + 30};
-            Shoot_left[i].speed_left.x = 3;
-            Shoot_left[i].radius_left = 10;
-            Shoot_left[i].active_left = false;
-            Shoot_left[i].color_left = BLUE;
-            Shoot_left[i].lifespawn_left = 0;
+        Shoot_left[i].position_left = (Vector2){playerData.pos.x + 30, playerData.pos.y + 30};
+        Shoot_left[i].speed_left.x = 3;
+        Shoot_left[i].radius_left = 10;
+        Shoot_left[i].active_left = false;
+        Shoot_left[i].color_left = BLUE;
+        Shoot_left[i].lifespawn_left = 0;
     }
+
     /* for(int i = 0; i < MAX_SHOTS; i++){
         shoot Shoot[i];
         
     } */
+
     int shoot_right = 0;
     
-    
     //Rectangle floor = (FloorX, FloorY, FloorW, FloorH);
+    
+    while (!WindowShouldClose()){
 
+        // Pause menu
+        int pausemenu_option = 1;
+        if(IsKeyReleased(KEY_BACKSPACE)){
+            PauseSound(gameplay_background_audio);
+            pausemenu_option = pause(width, height);
+            if(pausemenu_option==RESUME) ResumeSound(gameplay_background_audio);
+            else if(pausemenu_option==MENU) return MENU;
+            else if(pausemenu_option==QUIT) return QUIT;
+        }
 
-    while (!WindowShouldClose())
-    {
         // Frame counter for animation incrementation
         framesCounter++;
 
@@ -180,85 +194,79 @@ int windowDimensions[2];
         Rectangle BluePlatform = {windowDimensions[0]/2 - 25, windowDimensions[1] - 50, 50, 50};
         Rectangle YellowPlatform = {windowDimensions[0]/2 + 120, windowDimensions[1] - 220, 125, 50};
         
-        
         for(int i = 0; i < MAX_SHOTS; i++){
         }
+
         if(!isOnGround(playerData, windowDimensions[1])){
             playerData.pos.y += gravity;
-        };
+        }
+        
         if(CheckCollisionRecs(foot, BluePlatform)){
-            
             playerData.pos.y = windowDimensions[1] - BluePlatform.height - playerData.rec.height - 1;
             acceleration = 0;
-            gravity = 0; 
-
+            gravity = 0;
         }
+        
         if(CheckCollisionRecs(foot, YellowPlatform)){
-            
             playerData.pos.y = YellowPlatform.y - playerData.rec.height - 1;
             acceleration = 0;
             gravity = 0;
         }
 
         // controles basicos 
-        if (IsKeyDown(KEY_D))
-        {
+        if (IsKeyDown(KEY_D)){
             initAnim = 0;
             endAnim = 4;
             shoot_right = 1;
             playerData.pos.x += 10;
             
-            if (framesCounter >= (60/framesSpeed))
-        {
+            if (framesCounter >= (60/framesSpeed)){
                 framesCounter = 0;
                 currentFrame++;
-
-                 if (currentFrame > endAnim || currentFrame < initAnim) currentFrame = initAnim;
-
-                playerData.rec.x = (float)currentFrame*(float)playerData.rec.width;
-        }
-        }
-
-        if (IsKeyDown(KEY_A))
-        {
-            initAnim = 11;
-            endAnim = 15;
-
-            if (framesCounter >= (60/framesSpeed))
-        {
-                framesCounter = 0;
-                currentFrame++;
-
+                
                 if (currentFrame > endAnim || currentFrame < initAnim) currentFrame = initAnim;
 
                 playerData.rec.x = (float)currentFrame*(float)playerData.rec.width;
+            }
         }
-
+        
+        if (IsKeyDown(KEY_A)){
+            initAnim = 11;
+            endAnim = 15;
+            
+            if (framesCounter >= (60/framesSpeed)){
+                framesCounter = 0;
+                currentFrame++;
+                
+                if (currentFrame > endAnim || currentFrame < initAnim) currentFrame = initAnim;
+                
+                playerData.rec.x = (float)currentFrame*(float)playerData.rec.width;
+            }
+            
             shoot_right = 0;
             playerData.pos.x -= 10;
         }
+        
         if(IsKeyDown(KEY_W) && ((isOnGround(playerData, windowDimensions[1]) || CheckCollisionRecs(foot, YellowPlatform) || CheckCollisionRecs(foot, BluePlatform)))){
-
             gravity -= 30;
             playerData.pos.y -= 1;
-
+            
             if(shoot_right){
                 initAnim = 6;
                 endAnim = 6;
-
                 playerData.rec.x = 6*(float)playerData.rec.width;
             }
-
+            
             if(!shoot_right){
                 initAnim = 8;
                 endAnim = 8;
-
                 playerData.rec.x = 8*(float)playerData.rec.width;
             }
-        }else if (IsKeyDown(KEY_W) && (!isOnGround(playerData, windowDimensions[1]) || !CheckCollisionRecs(foot, YellowPlatform) || !CheckCollisionRecs(foot, BluePlatform)) && gravity >= 1)
-        {
+        }
+        else if (IsKeyDown(KEY_W) && (!isOnGround(playerData, windowDimensions[1]) || !CheckCollisionRecs(foot, YellowPlatform) || !CheckCollisionRecs(foot, BluePlatform)) && gravity >= 1){
             acceleration = 0.18;
-        }else{
+        }
+        else{
             if(!isOnGround(playerData, windowDimensions[1]) && !CheckCollisionRecs(foot, YellowPlatform) && !CheckCollisionRecs(foot, BluePlatform))
             acceleration = 1.5;
         }
@@ -269,29 +277,28 @@ int windowDimensions[2];
             if (shoot_right) {
             initAnim = 0;
             endAnim = 0;
-
-            playerData.rec.x = 0*(float)playerData.rec.width; 
+            playerData.rec.x = 0*(float)playerData.rec.width;
             }
 
             if (!shoot_right) {
             initAnim = 11;
             endAnim = 11;
-
-            playerData.rec.x = 11*(float)playerData.rec.width; 
-            }    
-        } 
+            playerData.rec.x = 11*(float)playerData.rec.width;
+            }
+        }
+        
         gravity += acceleration;
-
+        
         // Tiro do player
         if (IsKeyPressed(KEY_SPACE) && shoot_right == 1)
         {
-             for(int i = 0; i < MAX_SHOTS; i++){
-                 if(!Shoot[i].active){
-                     Shoot[i].position = (Vector2){playerData.pos.x + 30, playerData.pos.y + 30};
-                     Shoot[i].active = true;
-                     break;
-                 }
-             }
+            for(int i = 0; i < MAX_SHOTS; i++){
+                if(!Shoot[i].active){
+                    Shoot[i].position = (Vector2){playerData.pos.x + 30, playerData.pos.y + 30};
+                    Shoot[i].active = true;
+                    break;
+                }
+            }
         }
 
         for(int i = 0; i < MAX_SHOTS; i++){
@@ -302,18 +309,14 @@ int windowDimensions[2];
                     Shoot[i].active = false;
                 }
                 if(Shoot[i].active){
-                initAnim = 0;
-                endAnim = 4;
-
-                if (framesCounter >= (60/framesSpeed))
-                {
-                    framesCounter = 0;
-                    currentFrame++;
-
-                    if (currentFrame > endAnim || currentFrame < initAnim) currentFrame = initAnim;
-
-                    playerData.rec.x = (float)currentFrame*(float)playerData.rec.width;
-                }
+                    initAnim = 0;
+                    endAnim = 4;
+                    if (framesCounter >= (60/framesSpeed)){
+                        framesCounter = 0;
+                        currentFrame++;
+                        if (currentFrame > endAnim || currentFrame < initAnim) currentFrame = initAnim;
+                        playerData.rec.x = (float)currentFrame*(float)playerData.rec.width;
+                    }
                 }
                 if(Shoot[i].lifespawn >= 800){
                     Shoot[i].position = (Vector2){playerData.pos.x + 30, playerData.pos.y + 30};
@@ -325,16 +328,16 @@ int windowDimensions[2];
         }
         if (IsKeyPressed(KEY_SPACE) && shoot_right == 0)
         {
-             for(int i = 0; i < MAX_SHOTS_LEFT; i++){
-                 if(!Shoot_left[i].active_left){
-                     Shoot_left[i].position_left = (Vector2){playerData.pos.x + 30, playerData.pos.y + 30};
-                     Shoot_left[i].active_left = true;
-                     break;
-                 }
-             }
+            for(int i = 0; i < MAX_SHOTS_LEFT; i++){
+                if(!Shoot_left[i].active_left){
+                    Shoot_left[i].position_left = (Vector2){playerData.pos.x + 30, playerData.pos.y + 30};
+                    Shoot_left[i].active_left = true;
+                    break;
+                }
+            }
         }
         
-        for(int i = 0; i < MAX_SHOTS_LEFT; i++){  
+        for(int i = 0; i < MAX_SHOTS_LEFT; i++){
             if(Shoot_left[i].active_left){
                 Shoot_left[i].position_left.x -= 12;
                 Shoot_left[i].lifespawn_left++;
@@ -372,26 +375,31 @@ int windowDimensions[2];
         /* if(onFloor){
             playerData.pos.y = windowDimensions[1] + playerData.rec.y;
         } */
-         BeginDrawing();
-        ClearBackground(WHITE);
+        
+        // Gameplay Drawing
+        BeginDrawing();
+        DrawTexture(gameplay_background, 0, 0, WHITE);
+
         DrawTextureRec(
             player,
             playerData.rec,
             playerData.pos,
-            WHITE);      
-         DrawTextureRec(
+            WHITE);
+        
+        DrawTextureRec(
             memory,
             memoryData.rec,
             memoryData.pos,
-            WHITE);      
-
-
+            WHITE);
+        
         DrawRectangle(BluePlatform.x, BluePlatform.y, BluePlatform.width, BluePlatform.height, RED);
         DrawRectangle(YellowPlatform.x, YellowPlatform.y, YellowPlatform.width, YellowPlatform.height, YELLOW);
         EndDrawing();
+        
+        // Check if the background sound is playing
+        if(!IsSoundPlaying(gameplay_background_audio)) PlaySound(gameplay_background_audio);
     }
 }
-
 
 // Pause menu
 int pause(int width, int height){
@@ -555,7 +563,6 @@ int death(int width, int height){
     CloseAudioDevice();
 }
 
-
 // Game menu
 int menu(int width, int height){
     // Menu Variables
@@ -630,10 +637,14 @@ int main(void){
     const int height = 720;
     InitWindow(width, height, "A Code Adventure");
 
+    // Game Window Icon
+    Image game_icon = LoadImage("images/game_icon.png");
+    SetWindowIcon(game_icon);
+    
     // Animation aux
-    int framesSpeed = 6;  
+    int framesSpeed = 6;
     int framesCounter = 0;
-
+    
     // Player Animation
     
     // Game FPS
